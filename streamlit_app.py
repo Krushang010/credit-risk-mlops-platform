78fec608291c053e1832c8b881d472b2f8b92a20
Krushang010 <kushnjr11@gmail.com>
2026-05-09 00:55:37 +0530

    Added frontend API debugging

--- streamlit_app.py
+++ streamlit_app.py
@@ -155,6 +155,8 @@ if page == "🔍 Loan Prediction":
                     json=payload,
                     timeout=20
                 )
+                st.write("Status Code:", response.status_code)
+                st.write("Response Text:", response.text)
 
             except requests.exceptions.RequestException as e:
 
